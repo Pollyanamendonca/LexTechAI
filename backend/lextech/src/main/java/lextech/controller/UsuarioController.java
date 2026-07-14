f23f758929061699b570f14b30b516359573af11
Pollyana Mendonça <pollyanamend@gmail.com>
2026-07-14 20:03:20 +0200

    Lección 16 - Crear UsuarioController

---
+++ backend/lextech/src/main/java/lextech/controller/UsuarioController.java
@@ -0,0 +1,27 @@
+package lextech.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lextech.model.Usuario;
+import lextech.service.UsuarioService;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioService.listarUsuarios();
+    }
+
+}

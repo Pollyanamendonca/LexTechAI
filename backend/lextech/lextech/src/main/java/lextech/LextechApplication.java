1fabad76a16c81b311f969b8b066c34e40fba88c
Pollyana Mendonça <pollyanamend@gmail.com>
2026-07-14 13:22:11 +0200

    Lección 11 - Crear proyecto Spring Boot

--- backend/lextech/lextech/src/main/java/lextech/LextechApplication.java
+++ backend/lextech/lextech/src/main/java/lextech/LextechApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LextechApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LextechApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LextechApplication.class, args);
+    }
 
 }

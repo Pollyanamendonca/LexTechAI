d17a4d023c3fc67d82ccc1817f7fcefc588ec2dc
Pollyana Mendonça <pollyanamend@gmail.com>
2026-07-14 19:14:28 +0200

    Lección 14 - Crear UsuarioRepository

---
+++ backend/lextech/src/main/java/lextech/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package lextech.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lextech.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+}

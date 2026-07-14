88165c1c2cd1fc6fe06186840ed8128b76ca0605
Pollyana Mendonça <pollyanamend@gmail.com>
2026-07-14 19:22:24 +0200

    Lección 15 - Crear UsuarioService

---
+++ backend/lextech/src/main/java/lextech/service/UsuarioService.java
@@ -0,0 +1,38 @@
+package lextech.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lextech.model.Usuario;
+import lextech.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Optional<Usuario> buscarPorEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+
+    public Usuario guardarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void eliminarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+}

2c98094ce41406e69572484db96781dad77e2f20
Pollyana Mendonça <pollyanamend@gmail.com>
2026-07-14 12:34:24 +0200

    Lección 9 - Subida de contratos

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,20 @@
 console.log("LexTech AI iniciado correctamente.");
+const pdf = document.getElementById("pdf");
+
+if(pdf){
+
+pdf.addEventListener("change",function(){
+
+const archivo = pdf.files[0];
+
+if(archivo){
+
+document.getElementById("nombreArchivo").innerHTML=
+
+archivo.name;
+
+}
+
+});
+
+}

9b8d41ba1bfa74707f6ed1f3c082b90a558035d1
Pollyana Mendonça <pollyanamend@gmail.com>
2026-07-15 17:39:19 +0200

    leccion 20

--- backend/lextech/src/main/java/lextech/config/SecurityConfig.java
+++ backend/lextech/src/main/java/lextech/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package lextech.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +15,21 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/usuarios/**").permitAll()
+                .anyRequest().permitAll()
+            )
+
+            .httpBasic(Customizer.withDefaults())
+
+            .formLogin(form -> form.disable());
+
+        return http.build();
+    }
 }

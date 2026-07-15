15cb90a6711101f9fe41babf04af073d7a95828e
Pollyana Mendonça <pollyanamend@gmail.com>
2026-07-15 22:47:04 +0200

    Lección 26 completada - Lectura de PDF con PDFBox

--- backend/lextech/src/main/java/lextech/controller/ContratoController.java
+++ backend/lextech/src/main/java/lextech/controller/ContratoController.java
@@ -9,42 +9,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 @RestController
 @RequestMapping("/api/contratos")
 @CrossOrigin(origins = "*")
 public class ContratoController {
 
-   @PostMapping("/upload")
-public ResponseEntity<String> subirContrato(
-        @RequestParam("archivo") MultipartFile archivo) {
+    @PostMapping("/upload")
+    public ResponseEntity<String> subirContrato(
+            @RequestParam("archivo") MultipartFile archivo) {
 
-    System.out.println("===== SUBIDA DE CONTRATO =====");
-    System.out.println("Nombre: " + archivo.getOriginalFilename());
-    System.out.println("Vacío: " + archivo.isEmpty());
+        System.out.println("===== SUBIDA DE CONTRATO =====");
+        System.out.println("Nombre: " + archivo.getOriginalFilename());
+        System.out.println("Vacío: " + archivo.isEmpty());
 
-    try {
+        try {
 
-       File carpeta = new File("C:/Users/sipho/Desktop/LexTechAI/backend/lextech/uploads");
+            File carpeta = new File("C:/Users/sipho/Desktop/LexTechAI/backend/lextech/uploads");
 
-        if (!carpeta.exists()) {
-            carpeta.mkdirs();
+            if (!carpeta.exists()) {
+                carpeta.mkdirs();
+            }
+
+            File destino = new File(carpeta, archivo.getOriginalFilename());
+
+            archivo.transferTo(destino);
+
+            PDDocument documento = Loader.loadPDF(destino);
+
+            PDFTextStripper lector = new PDFTextStripper();
+
+            String texto = lector.getText(documento);
+
+            System.out.println("========== CONTENIDO DEL PDF ==========");
+            System.out.println(texto);
+
+            documento.close();
+
+            return ResponseEntity.ok("Contrato subido correctamente.");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            return ResponseEntity.badRequest()
+                    .body("Error: " + e.getMessage());
+
         }
-
-        File destino = new File(carpeta, archivo.getOriginalFilename());
-
-        archivo.transferTo(destino);
-
-        return ResponseEntity.ok("Contrato subido correctamente.");
-
-    } catch (Exception e) {
-
-        e.printStackTrace();
-
-        return ResponseEntity.badRequest()
-                .body("Error: " + e.getMessage());
-
     }
-}
 
 }
